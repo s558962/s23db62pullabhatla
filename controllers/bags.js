c330dc70ca9883278cf9cad102643ca545c67d75
Sai Krishna P <123139223+s558962@users.noreply.github.com>
2023-04-19 18:40:08 -0500

    Added new pug files in views

--- controllers/bags.js
+++ controllers/bags.js
@@ -30,13 +30,27 @@ exports.bags_create_post = async function(req, res) {
     }
     };
 // Handle bags delete form on DELETE.
-exports.bags_delete = function(req, res) {
+/*exports.bags_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: bags delete DELETE ' + req.params.id);
-};
+};*/
+
 // Handle bags update form on PUT.
-exports.bags_update_put = function(req, res) {
+/*exports.bags_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: bags update PUT' + req.params.id);
-};// List of all bagss
+};// List of all bagss*/
+
+// Handle bags delete on DELETE.
+exports.bags_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await bags.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
 
 // List of all bags
 exports.bags_list = async function(req, res) {
@@ -86,3 +100,92 @@ exports.bags_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 };
+
+// for a specific bags.
+exports.bags_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await bags.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
+};
+
+//Handle bags update form on PUT.
+exports.bags_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await bags.findById( req.params.id)
+// Do updates of properties
+if(req.body.bags_type)
+toUpdate.bags_name = req.body.bags_name;
+if(req.body.bags_type) toUpdate.bags_type = req.body.bags_type;
+if(req.body.bags_cost) toUpdate.bags_cost = req.body.bags_cost;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
+
+// Handle a show one view with id specified by query
+exports.bags_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await bags.findById( req.query.id)
+    res.render('bagsdetail',
+    { title: 'bags Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+// Handle building the view for creating a bags.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.bags_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('bagscreate', { title: 'bags Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle building the view for updating a bags.
+// query provides the id
+exports.bags_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await bags.findById(req.query.id)
+    res.render('bagsupdate', { title: 'bags Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+
+// Handle a delete one view with id from query
+exports.bags_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await bags.findById(req.query.id)
+    res.render('bagsdelete', { title: 'bags Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/bags.js
+++ routes/bags.js
@@ -3,4 +3,12 @@ const bags_controlers= require('../controllers/bags');
 var router = express.Router();
 /* GET bagss */
 router.get('/', bags_controlers.bags_view_all_Page );
+/* GET detail bags page */
+router.get('/detail', bags_controlers.bags_view_one_Page);
+/* GET create bags page */
+router.get('/create', bags_controlers.bags_create_Page);
+/* GET create update page */
+router.get('/update', bags_controlers.bags_update_Page);
+
+router.get('/delete', bags_controlers.bags_delete_Page);
 module.exports = router;
