bb770edaa6541a16aad79ba27abf837751a163f5
Sai Krishna P <123139223+s558962@users.noreply.github.com>
2023-04-27 22:27:51 -0500

    End

--- routes/bags.js
+++ routes/bags.js
@@ -1,14 +1,23 @@
 var express = require('express');
 const bags_controlers= require('../controllers/bags');
 var router = express.Router();
+
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
+
 /* GET bagss */
 router.get('/', bags_controlers.bags_view_all_Page );
 /* GET detail bags page */
-router.get('/detail', bags_controlers.bags_view_one_Page);
+router.get('/detail',secured,  bags_controlers.bags_view_one_Page);
 /* GET create bags page */
-router.get('/create', bags_controlers.bags_create_Page);
+router.get('/create', secured, bags_controlers.bags_create_Page);
 /* GET create update page */
 router.get('/update', bags_controlers.bags_update_Page);
 
-router.get('/delete', bags_controlers.bags_delete_Page);
+router.get('/delete',secured, bags_controlers.bags_delete_Page);
 module.exports = router;
