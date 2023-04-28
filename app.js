99774d1182c3efef427bcb29f7221d5c3f372d3e
Sai Krishna P <123139223+s558962@users.noreply.github.com>
2023-04-27 22:28:01 -0500

    End

--- app.js
+++ app.js
@@ -59,6 +59,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -68,13 +78,6 @@ app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
 
-app.use(require('express-session')({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
- }));
- app.use(passport.initialize());
- app.use(passport.session());
 
 async function recreateDB(){
   // Delete everything
--- models/bags.js
+++ models/bags.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const bagsSchema = mongoose.Schema({
-    bags_name: String,
+    bags_name: {type : String, minlength : 1, maxlength:50},
     bags_type: String,
-    bags_cost: String
+    bags_cost: {type : String, minlength : 1, maxlength:50}
 })
 
 module.exports = mongoose.model("bags",bagsSchema)
